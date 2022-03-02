d6e91747e3e172275428ae7196900b6ca5264961
Theodore Norvell <theo@mun.ca>
2022-03-01 23:34:47 -0330

    fix problems with fooWrapper.c

--- fooWrapper.c
+++ fooWrapper.c
@@ -1,13 +1,22 @@
 typedef int bool_t ;
+const int true = 1;
+const int false = 0 ;
 
 typedef enum event_e {
     TICK, a 
-} event_t ;
+} event_class_t ;
+
 typedef struct event_s {
     event_class_t event_class ;
 } event_t ;
 
-#define eventClassOr( evp ) (evp->event_class)
+#define eventClassOf( evp ) (evp->event_class)
+
+typedef int status_t ;
+#define OK_STATUS (0)
+#define OK( s ) ( (s)==OK_STATUS )
+
+#define assertUnreachable() (0)
 
 typedef int localIndex_t ;
 
